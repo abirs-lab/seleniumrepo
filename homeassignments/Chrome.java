90a0cfdff7ef42a3e7a84384d532922228e11006
abirs-lab <kamil4abeer@gmail.com>
2024-09-21 14:15:53 +0530

    Add files via upload
    week1day1 assignments

---
+++ homeassignments/Chrome.java
@@ -0,0 +1,20 @@
+package week1.homeassignments;
+
+public class Chrome {
+
+	public static void main(String[] args)
+	{
+		float version = 91.0f;
+		String developer = "Google";
+		boolean beta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'c';
+
+		System.out.println("The Browser version is:" + version + "\nThe Developer name is:" + developer
+				+ "\nis the Browser Beta version:" + beta);
+		System.out.println("The Release year is:" + releaseYear);
+		System.out.println("The shortcut key is:" + shortcutKey);
+
+	}
+
+}
